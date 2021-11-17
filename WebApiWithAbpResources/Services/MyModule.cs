6d8b067e394913d1d6084360bf265924591df663
omrs2002 <omrs2002@yahoo.com>
2021-11-17 13:34:36 +0300

    Add Turkish

--- WebApiWithAbpResources/Services/MyModule.cs
+++ WebApiWithAbpResources/Services/MyModule.cs
@@ -1,17 +1,7 @@
-﻿//using Localization.Resources.AbpUi;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Volo.Abp;
-//using Volo.Abp.AspNetCore.Mvc;
-//using Volo.Abp.AspNetCore.Mvc.Localization;
-//using Volo.Abp.Autofac;
-using Volo.Abp.Localization;
+﻿using Volo.Abp.Localization;
 using Volo.Abp.Localization.ExceptionHandling;
 using Volo.Abp.Modularity;
-//using Volo.Abp.Validation.Localization;
 using Volo.Abp.VirtualFileSystem;
-
 
 namespace WebApiWithAbpResources
 {
@@ -34,8 +24,6 @@ namespace WebApiWithAbpResources
                     //.AddBaseTypes(typeof(AbpValidationResource)) //Inherit from an existing resource
                     .AddVirtualJson("/Localization/Resources/Test")
                     ;
-                //options.Languages.Add(new LanguageInfo("ar", "ar", "العربية"));
-                //options.Languages.Add(new LanguageInfo("en", "en", "English"));
                 options.DefaultResourceType = typeof(TestResource);
             });
 
